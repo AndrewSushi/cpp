697479e618f2cf72f604c0ace0b140aa2ea752f4
AndrewSushi <AndrewSushi8@gmail.com>
2023-02-12 20:33:37 -0800

    Made a guessing game

---
+++ Projects/guessingGame.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void guessingGame(){
+    int guesses = 0;
+    int number = rand() % 100 + 1;
+    int guess;
+
+    cout << "The goal of this game is to guess the number between 1 and 100!" << endl;
+    while(1){
+        cout << "Current Guesses: " << guesses << endl;
+        cout << "Guess a number between 1 and 100:" << endl;
+        cin >> guess;
+        if(guess == number){
+            cout << "Correct! It took " << guesses << endl;
+            return;
+        }else if(guess < number){
+            cout << "Incorrect! The number is higher!" << endl;
+            cout << endl;
+            guesses++;
+        }else{
+            cout << "Incorrect! The number is lower!" << endl;
+            cout << endl;
+            guesses++;
+        }
+    }
+}
+
+int main(){
+    guessingGame();
+    return 0;
+}
